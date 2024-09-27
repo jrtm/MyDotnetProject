3ff95f80b3fd8ed8c5316ddb23ccfa74745ab52d
Julian Ravn Thrap-Meyer <jr@thrap-meyer.no>
2024-09-27 15:33:58 +0200

    Add really good admin controller, yep

---
+++ MyDotnetAPI/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyDotnetAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+//[Authorize]
+public class AdminController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] string path)
+    {
+        using StreamReader reader = new(path);
+        var text = reader.ReadToEnd();
+        
+        return Ok("Here you go: " + text);
+    }
+    
+    [HttpPost]
+    public IActionResult Post([FromBody] AdminAction action)
+    {
+        return Ok("All OK!");
+    }
+    
+}
+
+public record AdminAction(string Action);
